b3605cbb6535e91567ef93e8cddfeaabb91acc46
Sistemas digitalDot <164342086+sistemasdD@users.noreply.github.com>
2024-09-11 10:27:05 +0200

    Create acl_cobian.bash

---
+++ acl_cobian.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+setfacl -m u:backupdd:x /var/lib/psa/dumps/
+setfacl -m mask::x /var/lib/psa/dumps/
+setfacl -m u:backupdd:rwx /var/lib/psa/dumps/domains/
+setfacl -m default:u:backupdd:rwx /var/lib/psa/dumps/domains/
+setfacl -m mask::rwx /var/lib/psa/dumps/domains/
+setfacl -m default:mask::rwx /var/lib/psa/dumps/domains/
+
+shopt -q globstar ; _globstarStatus=$?
+(( _globstarStatus )) && shopt -s globstar
+setfacl -m u:backupdd:rwx /var/lib/psa/dumps/domains/**
+setfacl -m default:u:backupdd:rwx /var/lib/psa/dumps/domains/**/
+setfacl -m mask::rwx /var/lib/psa/dumps/domains/**
+setfacl -m default:mask::rwx /var/lib/psa/dumps/domains/**/
+(( _globstarStatus )) && shopt -u globstar
